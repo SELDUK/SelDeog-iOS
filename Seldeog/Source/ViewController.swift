5ce8868f2a9f187b8a81276406453624bd816dc9
권준상 <wnstkdsun97@naver.com>
2022-02-18 15:46:43 +0900

    폴더 정리

---
+++ Seldeog/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seldeog
+//
+//  Created by 권준상 on 2022/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
