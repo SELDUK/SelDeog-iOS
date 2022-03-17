1e1eed707289a09875da6db78761a536dfce14ae
권준상 <wnstkdsun97@naver.com>
2022-03-17 16:29:01 +0900

    Image 추가

--- Seldeog/Resource/Image.swift
+++ Seldeog/Resource/Image.swift
@@ -8,6 +8,7 @@
 import UIKit.UIImage
 
 enum Image {
+    // 기본 아이콘
     static let arrowDown = UIImage(named: "ic_arrow_down_line")!
     static let arrowLeft = UIImage(named: "ic_arrow_left_line")!
     static let arrowRight = UIImage(named: "ic_arrow_right_line")!
@@ -15,4 +16,18 @@ enum Image {
     static let personFilled = UIImage(named: "ic_person_filled")!
     static let personLine = UIImage(named: "ic_person_line")!
     static let xLine = UIImage(named: "ic_x_line")!
+    
+    // 캐릭터 모양
+    static let shapeCircle = UIImage(named: "shape_circle")!
+    static let shapeDent = UIImage(named: "shape_dent")!
+    static let shapeCloud = UIImage(named: "shape_cloud")!
+    static let shapeHeart = UIImage(named: "shape_heart")!
+    static let shapeUglyHeart = UIImage(named: "shape_uglyHeart")!
+    static let shapeRock = UIImage(named: "shape_rock")!
+    
+    // 캐릭터 표정
+    static let expressionSmile = UIImage(named: "expression_smile")!
+    static let expressionSad = UIImage(named: "expression_sad")!
+    static let expressionConfidence = UIImage(named: "expression_confidence")!
+    static let expressionBlank = UIImage(named: "expression_blank")!
 }
