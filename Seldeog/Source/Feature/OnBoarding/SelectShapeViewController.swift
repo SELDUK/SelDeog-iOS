8e378d28b4f433c1aa353ffe037e43c500c46369
권준상 <wnstkdsun97@naver.com>
2022-03-20 15:25:53 +0900

    캐릭터 모양 선택 화면 구현

--- Seldeog/Source/Feature/OnBoarding/SelectShapeViewController.swift
+++ Seldeog/Source/Feature/OnBoarding/SelectShapeViewController.swift
@@ -16,6 +16,7 @@ final class SelectShapeViewController: BaseViewController {
     let titleLabel = UILabel()
     let characterImageView = UIImageView()
     let expressionImageView = UIImageView()
+    let nextButton = UIButton()
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -24,12 +25,37 @@ final class SelectShapeViewController: BaseViewController {
         return cv
     }()
     
+    var cellImageList = [Image.shapeCircle, Image.shapeHeart, Image.shapeDent, Image.shapeRock, Image.shapeCloud, Image.shapeUglyHeart]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setProperties()
         setLayouts()
     }
     
+}
+
+extension SelectShapeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 80, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MakeCharacterCell", for: indexPath) as? MakeCharacterViewCell else { return UICollectionViewCell() }
+        
+        cell.setImage(image: cellImageList[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+        self.characterImageView.image = cellImageList[indexPath.item]
+    }
 }
 
 extension SelectShapeViewController {
@@ -57,20 +83,33 @@ extension SelectShapeViewController {
         titleLabel.do {
             $0.text = "1. SHAPE"
             $0.textColor = UIColor.black
-            $0.font = .nanumPen(size: 20, family: .bold)
+            $0.font = .nanumPen(size: 25, family: .bold)
         }
         
         characterImageView.do {
             $0.image = Image.shapeCircle
+            $0.contentMode = .scaleToFill
         }
         
         expressionImageView.do {
             $0.image = Image.expressionBlank
+            $0.contentMode = .scaleToFill
         }
         
         collectionView.do {
+            $0.register(MakeCharacterViewCell.self, forCellWithReuseIdentifier: "MakeCharacterCell")
+            $0.delegate = self
+            $0.dataSource = self
+            $0.backgroundColor = UIColor(patternImage: Image.checkPattern)
             $0.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
             $0.showsHorizontalScrollIndicator = false
+        }
+        
+        nextButton.do {
+            $0.setTitle("OK", for: .normal)
+            $0.setTitleColor(.white, for: .normal)
+            $0.setBackgroundColor(.black, for: .normal)
+            $0.titleLabel?.font = .nanumPen(size: 25, family: .bold)
         }
         
     }
@@ -81,7 +120,8 @@ extension SelectShapeViewController {
     }
     
     private func setViewHierarchy() {
-        view.addSubViews(myCharacterLabel, loadingBar, titleLabel, characterImageView, expressionImageView, collectionView)
+        view.addSubViews(myCharacterLabel, loadingBar, titleLabel, characterImageView, collectionView, nextButton)
+        characterImageView.addSubview(expressionImageView)
         characterImageView.bringSubviewToFront(expressionImageView)
     }
     
@@ -98,6 +138,34 @@ extension SelectShapeViewController {
             $0.leading.equalTo(safeArea).offset(54)
             $0.width.equalTo(74)
             $0.height.equalTo(29)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(loadingBar.snp.bottom).offset(35)
+            $0.centerX.equalToSuperview()
+        }
+        
+        characterImageView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(35)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(250)
+        }
+        
+        expressionImageView.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.width.height.equalTo(250)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(characterImageView.snp.bottom).offset(20)
+            $0.leading.trailing.equalTo(safeArea)
+            $0.height.equalTo(120)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalTo(safeArea)
+            $0.height.equalTo(80)
         }
     }
 }
